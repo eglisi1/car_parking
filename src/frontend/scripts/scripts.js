eb34ab16c81d61741fdae1c20792e6ca48088744
Simon Egli <eglisi1@students.zhaw.ch>
2023-11-16 13:20:10 +0100

    add simple frontend

---
+++ src/frontend/scripts/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('takePictureBtn').addEventListener('click', function() {
+    alert('Take Picture button clicked!');
+});
+
+document.getElementById('dashboardBtn').addEventListener('click', function() {
+    alert('Dashboard button clicked!');
+});
