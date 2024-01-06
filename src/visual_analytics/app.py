bd5cc63e3357b4e883b6675adf35dac097cf7f91
Simon Egli <eglisi1@students.zhaw.ch>
2024-01-06 15:52:50 +0100

    simplify app.py

--- src/visual_analytics/app.py
+++ src/visual_analytics/app.py
@@ -11,6 +11,14 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+
+def parse_request_dates():
+    default_from_date = datetime.today() - timedelta(days=7)
+    default_to_date = datetime.today()
+    date_from = util_service.parse_date(request.args.get("date_from"), default_from_date)
+    date_to = util_service.parse_date(request.args.get("date_to"), default_to_date)
+    return date_from, date_to
+
 @app.route("/")
 def index() -> str:
     logger.debug("/ called")
@@ -20,8 +28,7 @@ def index() -> str:
 @app.route("/average_occupancy")
 def occupancy_trends() -> str:
     logger.debug(f"/occupancy_trends called with args {request.args}")
-    date_from = util_service.parse_date(request.args.get("date_from", type=str), datetime.today() - timedelta(days=7))
-    date_to = util_service.parse_date(request.args.get("date_to", type=str), datetime.today())
+    date_from, date_to = parse_request_dates()
     base64_plot = analysis_service.occupancy_trends(date_from, date_to)
     return render_template("occupancy_trends_result.html", plot_url=base64_plot)
 
@@ -29,16 +36,14 @@ def occupancy_trends() -> str:
 @app.route("/occupancy_per_day")
 def occupancy_per_day() -> str:
     logger.debug(f"/occupancy_per_day called with args {request.args}")
-    date_from = util_service.parse_date(request.args.get("date_from", type=str), datetime.today() - timedelta(days=7))
-    date_to = util_service.parse_date(request.args.get("date_to", type=str), datetime.today())
+    date_from, date_to = parse_request_dates()
     base64_plot = analysis_service.occupancy_per_day(date_from, date_to)
     return render_template("occupancy_per_day_result.html", plot_url=base64_plot)
 
 @app.route("/occupancy_hours_by_parkingspace")
 def occupancy_hours_by_parkingspace() -> str:
     logger.debug(f"/occupancy_hours_by_parkingspace called with args {request.args}")
-    date_from = util_service.parse_date(request.args.get("date_from", type=str), datetime.today() - timedelta(days=7))
-    date_to = util_service.parse_date(request.args.get("date_to", type=str), datetime.today())
+    date_from, date_to = parse_request_dates()
     base64_plot = analysis_service.occupancy_hours_by_parkingspace(date_from, date_to)
     return render_template("occupancy_hours_by_parkingspace_result.html", plot_url=base64_plot)
 
