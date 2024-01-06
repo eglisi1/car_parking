da522b0cc41e2fc6f27db3e83332a20f576fbe57
Simon Egli <eglisi1@students.zhaw.ch>
2024-01-06 15:47:57 +0100

    remove unnecessary comments and rewrite necessary to english

--- src/visual_analytics/services/plot_service.py
+++ src/visual_analytics/services/plot_service.py
@@ -19,21 +19,18 @@ def create_plot_from_plt(plt: matplotlib.pyplot) -> str:
 def create_plot_occupancy_trends(trends_day: dict, trends_night: dict) -> str:
     plt.figure(figsize=(12, 6))
 
-    # Daten für das Plotten vorbereiten
     daten1 = [data[0] for data in trends_day]
     raten1 = [data[1] for data in trends_day]
     daten2 = [data[0] for data in trends_night]
     raten2 = [data[1] for data in trends_night]
 
-    # Zeichne beide Linien für die Zeiträume
     plt.plot(daten1, raten1, "r-", label="07:00-19:00")
     plt.plot(daten2, raten2, "b-", label="19:01-06:59")
 
-    # X-Achsen-Ticks auf maximal 10 beschränken
+    # X-Axis-Ticks limit to 10
     plt.gca().xaxis.set_major_locator(plt.MaxNLocator(10))
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%d %b %Y"))
 
-    # Beschriftungen und Diagramm-Formatierungen
     plt.ylim(0, 100)
     plt.title("Durchschnittliche Belegungsrate nach Tageszeit")
     plt.xlabel("Datum")
@@ -62,7 +59,6 @@ def create_plot_occupancy_per_day(average_per_day: dict) -> str:
         rates = [week_data.get(tag, 0) for tag in weekdays]
         plt.plot(weekdays, rates, label=daytime)
 
-    # Beschriftungen und Diagramm-Formatierungen
     plt.title("Durchschnittliche Belegungsrate pro Wochentag und Tageszeit")
     plt.xlabel("Wochentag")
     plt.ylabel("Belegungsrate (%)")
@@ -73,12 +69,22 @@ def create_plot_occupancy_per_day(average_per_day: dict) -> str:
 
     return create_plot_from_plt(plt)
 
-def create_plot_occupancy_hours_by_parkingspace(occupancy_hours_by_parkingspace: dict) -> str:
+
+def create_plot_occupancy_hours_by_parkingspace(
+    occupancy_hours_by_parkingspace: dict,
+) -> str:
     plt.figure(figsize=(10, 6))
-    plt.bar(range(len(occupancy_hours_by_parkingspace)), list(occupancy_hours_by_parkingspace.values()), align='center')
-    plt.xticks(range(len(occupancy_hours_by_parkingspace)), list(occupancy_hours_by_parkingspace.keys()))
-    plt.title('Besetzungsstunden pro Parkplatz')
-    plt.xlabel('Parkplatz ID')
-    plt.ylabel('Stunden')
+    plt.bar(
+        range(len(occupancy_hours_by_parkingspace)),
+        list(occupancy_hours_by_parkingspace.values()),
+        align="center",
+    )
+    plt.xticks(
+        range(len(occupancy_hours_by_parkingspace)),
+        list(occupancy_hours_by_parkingspace.keys()),
+    )
+    plt.title("Besetzungsstunden pro Parkplatz")
+    plt.xlabel("Parkplatz ID")
+    plt.ylabel("Stunden")
 
     return create_plot_from_plt(plt)
