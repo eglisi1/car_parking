508b5e4e94f9ca44b2ed2d0bf7db682da2bda8c8
Simon Egli <eglisi1@students.zhaw.ch>
2024-01-11 14:06:39 +0100

    relocate saschas work because it is crucial for our success. Thanks sascha

--- src/frontend/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('takePictureBtn').addEventListener('click', function() {
-    alert('Take Picture button clicked!');
-});
-
-document.getElementById('dashboardBtn').addEventListener('click', function() {
-    alert('Dashboard button clicked!');
-});
