03ed8f0036ad7eb8ce4f29a52553d8d54958c6f6
Nick Rascher <nick.rascher@hispeed.ch>
2024-01-11 15:15:22 +0100

    fix timestamp and crontab env vars

--- src/cronjobs/check_park_spaces.sh
+++ src/cronjobs/check_park_spaces.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /home/admin/cronlogs/env_vars.sh
 source /home/admin/dev/virtualenvs/cronvenv/bin/activate
 python /home/admin/dev/git_projects/car_parking/src/cronjobs/check_park_spaces_on_pi.py
 deactivate
